466f9253f1ed3a304ec8a9467dd8a533d52cbe8b
GraceE-Dion <efahnegbedion@gmail.com>
2026-03-29 17:07:45 -0400

    Create 03_automated_merge.py

---
+++ 03_automated_merge.py
@@ -0,0 +1,8 @@
+# STAGE 3: Automated Consolidation & Label Mapping
+# This script scans YOLO .txt files and moves images to a Master Directory
+# based on moisture levels 0-10.
+import os, shutil
+
+def merge_datasets(source_folders, master_dir):
+    # Logic to map '8.2' to '8' and move files
+    pass
