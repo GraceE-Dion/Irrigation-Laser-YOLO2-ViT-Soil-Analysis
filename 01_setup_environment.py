284aaef41b115f1ad58e493f3dacadf44801086b
GraceE-Dion <efahnegbedion@gmail.com>
2026-04-14 13:33:14 -0400

    Update 01_setup_environment.py

--- 01_setup_environment.py
+++ 01_setup_environment.py
@@ -1,5 +1,16 @@
 # STAGE 1: Environment Setup
 # Installs necessary AI libraries for Vision Transformers and Data Handling
-!pip install -q transformers datasets evaluate roboflow
-import torch
-print(f"GPU Detected: {torch.cuda.is_available()}")
+!pip install -q evaluate roboflow datasets transformers[torch]
+
+import os, shutil, yaml, torch, numpy as np, evaluate
+from roboflow import Roboflow
+from datasets import load_dataset
+from transformers import (
+    ViTImageProcessor,
+    ViTForImageClassification,
+    TrainingArguments,
+    DefaultDataCollator,
+    Trainer
+)
+from sklearn.metrics import classification_report, confusion_matrix
+import matplotlib.pyplot as plt
